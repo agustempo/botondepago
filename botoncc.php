943ee7db40b26574c8a32fba5ecf1fc11eed4e44
HarrySanti <HarrySanti@users.noreply.github.com>
2017-04-27 00:43:24 -0300

    Decimales corregido

--- botoncc.php
+++ botoncc.php
@@ -193,18 +193,64 @@
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
 			<script>
-				function valida(e){
+				function valida(e)
+				{
 					tecla = (document.all) ? e.keyCode : e.which;
-
-					//Tecla de retroceso para borrar, siempre la permite
-					if (tecla==8){
+					if (tecla==8)
+					{
 						return true;
 					}
-						
-					// Patron de entrada, en este caso solo acepta numeros
 					patron =/[0-9a-zA-Z]/;
 					tecla_final = String.fromCharCode(tecla);
 					return patron.test(tecla_final);
+				}
+			</script>
+			<script language="javascript">
+				function SoloNumerico(e, obj)
+				{
+					var sKey = -1;
+					var bResult = true;
+					var bPunto = (obj.value.indexOf(".") != -1);
+					var lCantChars = obj.value.length;
+					if (window.event)
+					{
+						sKey = e.keyCode;
+					}
+					else if (e.which)
+					{
+						sKey = e.which;
+					}
+					if (sKey > 10)
+					{
+						if (((sKey < 48 || sKey > 57) && (sKey != 46 || bPunto)) || (!bPunto &&	lCantChars > 6 && sKey != 46))
+						{
+							bResult = false;
+						}
+					}
+					return bResult;
+				}
+				function AplicoFormato(oText)
+				{
+					var aDec = oText.value.split('.');
+					if(aDec.length > 1) 
+					{
+						if(aDec[1].length == 1) 
+						{
+							oText.value = aDec[0] + '.' + aDec[1].split('.')[0] + '0';
+						}
+						if(aDec[1].length == 0) 
+						{
+							oText.value = aDec[0] + '.00';
+						}
+						if(aDec[1].length >= 2) 
+						{
+							oText.value = aDec[0] + '.' + aDec[1].substr(0,2);
+						}
+					}
+					else
+					{
+						oText.value = aDec + '.00';
+					}
 				}
 			</script>
 	</body>
