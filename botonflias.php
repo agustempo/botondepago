f600b6c64e39f0a017953e2b78c1c28c86c23df2
HarrySanti <HarrySanti@users.noreply.github.com>
2017-04-30 18:40:30 -0300

    Mejoras
    - Agregado descripcion (Familias, grupo o Staff)
    - Monto
    - Doble espacio
    - No me acuerdo que mas

--- botonflias.php
+++ botonflias.php
@@ -3,13 +3,17 @@
 <?php
 	$codigoDM = 1352279;
 	$show = true;
-	$tipo = $_GET['tipo'];
-	$nombre =$_GET['nombre'];
+	$tipo = $_GET['tipo'];	
 	$sede = $_GET['sede'];
 	$mes = $_GET['mes'];
 	$anio = $_GET['anio'];
-	$codigoCC = $tipo.' '.$nombre.' '.$sede.' '.$mes.' '.$anio;
+	$nombre =$_GET['nombre'];
+	$codigoCC = $tipo.' '.$sede.' '.$mes.' '.$anio.' '.$nombre;
 
+	$descripcionTipo['Fam'] = 'Familia';
+	$descripcionTipo['Gru'] = 'Grupo';
+	$descripcionTipo['Sta'] = 'Staff';
+	
   	$descripcionSede['BA'] = 'Buenos Aires';
 	$descripcionSede['BAN'] = 'BA Sede Norte';
 	$descripcionSede['BAO'] = 'BA Sede Oeste';
@@ -17,11 +21,22 @@
 	$descripcionSede['LP'] = 'La Plata';
 	$descripcionSede['ON'] = 'Oficina Nacional';
 	
-	$descripcionNombre['Fam'] = 'Familia';
-	$descripcionNombre['Gru'] = 'Grupo';
-	$descripcionNombre['Sta'] = 'Staff';
-	
-	$descripcionMonto[''] = '24000';
+	if ($tipo == 'Fam' || $tipo=='Gru' || $tipo=='Sta')
+	{
+		$montos['BA'] = "$24000";
+		$montos['BAN'] = "$24000";
+		$montos['BAO'] = "$24000";
+		$montos['BAS'] = "$24000";
+		$montos['LP'] = "$24000";
+		$montos['ON'] = "$24000";
+		
+		$mail['BA'] = 'familias.buenosaires@techo.org';
+		$mail['BAN'] = 'familias.buenosaires@techo.org';
+		$mail['BAO'] = 'familias.buenosaires@techo.org';
+		$mail['BAS'] = 'familias.buenosaires@techo.org';
+		$mail['LP'] = 'familias.buenosaires@techo.org';
+		$mail['ON'] = 'familias.buenosaires@techo.org';
+	}
 ?>
 
 <html>
@@ -41,7 +56,7 @@
 		<!-- Header -->
 			<header id="header">
 				<div class="content">
-					<h2>Bienvenido al Botón de Pago de TECHO</h2>
+					<h2>Bienvenido al Botón de Pago<br/> <?php echo $descripcionTipo[$tipo]?> <?php echo $nombre?> de TECHO</h2>
 					<ul class="actions">
 						<li><a href="#one" class="button icon fa-chevron-down scrolly">Mas Información</a></li>
 						<li><a href="#two" class="button icon fa-chevron-down scrolly">Ir a Pagar</a></li>					
@@ -53,10 +68,10 @@
 		<!-- One -->
 			<section id="one" class="wrapper style2 special">
 				<header class="major">
-					<h4><ul>La  Construcción con <?php echo Familias ?> es una oportunidad de voluntariado donde se participa de<br/> la construcción de una vivienda de emergencia junto a una familia que hoy vive en situación de extrema pobreza.</ul>
+					<h4><ul>La Construcción con <?php echo $descripcionTipo[$tipo]?> es una oportunidad de voluntariado donde se participa de<br/> la construcción de una vivienda de emergencia junto a una familia que hoy vive en situación de extrema pobreza.</ul>
 					<ul>La actividad, que transcurre durante un fin de semana completo, no está limitada a la edad ni a las habilidades<br/> en construcción de sus voluntarios, sino a sus ganas y compromiso con el trabajo comunitario.</ul>
 					<ul>Desde TECHO proveemos las herramientas, además de asegurarnos de que en todo momento se sientan acompañados<br/> por voluntarios que guiarán la construcción. Desde su lado, deberán financiar los materiales de la vivienda<br/> 
-					transfiriendo o depositando ARS$ <?php echo $descripcionMonto ?>, como así también llevar la comida para compartir<br/> el fin de semana con la familia a la que le construirán la vivienda.</ul></h4>										
+					transfiriendo o depositando ARS$ <?php echo $montos[$sede]?>, como así también llevar la comida para compartir<br/> el fin de semana con la familia a la que le construirán la vivienda.</ul></h4>										
 				</header>			
 			</section>
 
@@ -102,5 +117,53 @@
 			<script src="assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
+			<script language="javascript">
+				function SoloNumerico(e, obj)
+				{
+					var sKey = -1;
+					var bResult = true;
+					var bPunto = (obj.value.indexOf(".") != -1);
+					var lCantChars = obj.value.length;
+					if (window.event)
+					{
+						sKey = e.keyCode;
+					}
+					else if (e.which)
+					{
+						sKey = e.which;
+					}
+					if (sKey > 10)
+					{
+						if (((sKey < 48 || sKey > 57) && (sKey != 46 || bPunto)) || (!bPunto &&	lCantChars > 6 && sKey != 46))
+						{
+							bResult = false;
+						}
+					}
+					return bResult;
+				}
+				function AplicoFormato(oText)
+				{
+					var aDec = oText.value.split('.');
+					if(aDec.length > 1) 
+					{
+						if(aDec[1].length == 1) 
+						{
+							oText.value = aDec[0] + '.' + aDec[1].split('.')[0] + '0';
+						}
+						if(aDec[1].length == 0) 
+						{
+							oText.value = aDec[0] + '.00';
+						}
+						if(aDec[1].length >= 2) 
+						{
+							oText.value = aDec[0] + '.' + aDec[1].substr(0,2);
+						}
+					}
+					else
+					{
+						oText.value = aDec + '.00';
+					}
+				}
+			</script>
 	</body>
 </html>
