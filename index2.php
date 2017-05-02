c5258de0209b9d7501f0ae15811b52e6131c024b
Agus Vilas <agustin.vilas@techo.org.ar>
2017-05-02 12:30:39 -0300

    cambios para direccionar los pagos administracion.argentina

--- index2.php
+++ index2.php
@@ -29,7 +29,7 @@
 		$montos['BA'] = "entre $120 y $200";
 		$montos['MIS-OBE'] = "entre $80 y $100";
 		$montos['MIS-POS'] = "entre $80 y $100";
-		$montos['NQN-RN'] = "entre $65 y $90";
+		$montos['NQN-RN'] = "entre $65 y $100";
 		$montos['COCHA'] = "entre $100 y $140";
 		$montos['TUC'] = "entre $50 y $80";
 		$montos['CBA'] = "$100";
@@ -147,84 +147,50 @@
 			</div>
 
 
-				<form id="formulario" action="https://gateway.payulatam.com/ppp-web-gateway" method="post" target="_top">
 					<div style="float:left;color: rgb(47,149,173); font-weight: bold;font-size:17px; width:70%;padding-top:10px;padding-left:15px;">
 						<div style="width:40%;float:left;text-align:right;margin-right:10px;">Monto a Donar:</div>
 						<input type="text" id="item_ammount_1" name="item_ammount_1" value="" onKeyPress="return SoloNumerico(event, this);" Onblur="AplicoFormato(this)"/>
 						</br>
 						</br>
 						<div style="text-align:right;width:30%;float:left;margin-right:48px;">DNI:</div>
-						<input type="text" id="buyer_document_number" name="buyer_document_number"/>
+						<input type="text" id="payerDocument" name="payerDocument"/>
 					</div>
-					<input type="hidden" name="merchant" value="4083795" />
-	        <input type="hidden" name="payment_method_available" value="ar_dm;ar_visa;ar_master;ar_amex;ar_cencosud;ar_tnaranja;ar_tshopping;ar_argencard;ar_cabal;ar_bapropago;ar_cobroexpress;ar_rapipago;ar_pagofacil" />
-					<input type="hidden" id="item_name_1" name="item_name_1" value="<? echo $codigoFamilia; ?>" />
-					<input type="hidden" name="item_quantity_1" value="1" />
-					<input type="hidden" name="country_id" value="1" />
-					<input type="hidden" name="seller_name" value="Techo" />
-					<input type="hidden" name="header_image" value="https://argentina.dineromail.com/imagenes/LogosVendedores/1466897.gif"/></td></tr>
-					<div style="float:left;">
 
+					<form id="formulario" action="https://gateway.payulatam.com/ppp-web-gateway" method="post">
+						<input type="hidden" name="amount"  id="amount" value="">
+						<input type="hidden" id="merchantId" name="merchantId" value="536715">
+						<input type="hidden" id="referenceCode" name="referenceCode" value="CC-Voluntario">
+						<input type="hidden" id="accountId" name="accountId" value="538721">
+						<input type="hidden" id="description" name="description" value="<?php echo $codigoFamilia; ?>">
+						<input type="hidden" id="tax" name="tax" value="0">
+						<input type="hidden" id="taxReturnBase" name="taxReturnBase" value="0">
+						<input type="hidden" id="signature" name="signature" value="">
+						<input type="hidden" id="currency" name="currency" value="ARS">
+					</form>
 
-						<input type='submit' border=0 name=submit id="boton-donar" value="Pagar" style="font-size: 33px;
-	  font-weight: bold;
-	  color: white;
-	  /* border: 2px solid white; */
-	  background-color: rgb(47,149,173);
-	  padding: 10px 10px;
-	  margin-top: 10px;
-	  font-family: Din Light;"/>
+					<div style="float:right;">
+						<button type="button" onclick="enviar()" id="boton-donar" value="Pagar" style="    font-size: 33px;
+    font-weight: bold;
+    color: white;
+    background-color: rgb(47,149,173);
+    margin-left: 30px;
+    margin-top: 15px;
+    font-family: Din Light;
+    text-align: center;
+    margin-right: 10px;
+    width: 104px;
+">
+Pagar </buttom>
 					</div>
-				</form>
 
         <div style="margin-top:10px;color:rgb(47,149,173);font-size:17px;text-align:left;">
-          <br><br><br><br><p style="text-align:center;font-weight: bold;padding-bottom: 2ex;">Si quer&eacute;s participar y ten&eacute;s problemas para cubrir el pago (total o parcial), no lo dudes, solicitanos una BECA o PRORROGA enviando un mail a <?php echo $mail[$sede]; ?></p>
+          <br><br><br><br><br><p style="text-align:center;font-weight: bold;padding-bottom: 2ex;">Si quer&eacute;s participar y ten&eacute;s problemas para cubrir el pago (total o parcial), no lo dudes, solicitanos una BECA o PRORROGA enviando un mail a <?php echo $mail[$sede]; ?></p>
 	       </div>
 		</div>
-		<script language="javascript">
-		function SoloNumerico(e, obj)
-		{
-			var sKey = -1;
-			var bResult = true;
-			var bPunto = (obj.value.indexOf(".") != -1);
-			var lCantChars = obj.value.length;
-			if (window.event)
-			{
-				sKey = e.keyCode;
-			}
-			else if (e.which)
-			{
-				sKey = e.which;
-			}
-			if (sKey > 10)
-			{
-				if (((sKey < 48 || sKey > 57) && (sKey != 46 || bPunto)) || (!bPunto &&
-					lCantChars > 6 && sKey != 46))
-				{
-					bResult = false;
-				}
-			}
-			return bResult;
-		}
-		function AplicoFormato(oText)
-		{
-			var aDec = oText.value.split('.');
-			if(aDec.length > 1) {
-				if(aDec[1].length == 1) {
-					oText.value = aDec[0] + '.' + aDec[1].split('.')[0] + '0';
-				}
-				if(aDec[1].length == 0) {
-					oText.value = aDec[0] + '.00';
-				}
-				if(aDec[1].length >= 2) {
-					oText.value = aDec[0] + '.' + aDec[1].substr(0,2);
-				}
-			}
-			else
-			{
-				oText.value = aDec + '.00';
-			}
-		}
-	</script>
+
+		<script src="md5.js"></script>
+		<script src="index.js"></script>
+
+
 	</body>
 </html>
